e6241ebf23bdbac28b46a67a78e2d155c966da21
Adam Ribeiro <adam.ribeiro2013@gmail.com>
2026-05-15 15:08:53 -0300

    Correções + adição de tela de Login

--- inventory-app/resources/js/app.jsx
+++ inventory-app/resources/js/app.jsx
@@ -4,9 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Login from './Pages/Login';
-import Products from './Pages/Products/Products';
-import RequireAuth from './Auth/RequireAuth.jsx';
+import Login from './Login';
+import Products from './Products';
 
 ReactDOM.createRoot(document.getElementById('app')).render(
     <BrowserRouter>
